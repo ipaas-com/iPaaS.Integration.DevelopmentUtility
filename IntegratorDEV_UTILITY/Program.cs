ba91d9f90600860f7b239637ced8f33e60f5cf82
Vincent Valentine <marjak101@gmail.com>
2021-11-15 07:51:45 -0800

    Fixed Vulnerability

--- IntegratorDEV_UTILITY/Program.cs
+++ IntegratorDEV_UTILITY/Program.cs
@@ -24,7 +24,7 @@ namespace IntegrationDevelopmentUtility
             //Settings.LoggerUrl = "https://devapi.ipaas.com/listener";
             //Settings.SSOUrl = "https://devapi.ipaas.com/sso";
 
-            //Settings.AzureFileShareConnectionString = "DefaultEndpointsProtocol=https;AccountName=integrationdevshare;AccountKey=20Gy5XDKzIR8PkmpQGYQrFjIkbtfVwMM/yD1NF5z8Sfd9PH9JlGbdLz4F8t3b37eo/RkmM3VgZie20tQS70AJA==;EndpointSuffix=core.windows.net";
+            //Settings.AzureFileShareConnectionString = "DefaultEndpointsProtocol=https;AccountName=integrationdevshare;AccountKey="Key"==;EndpointSuffix=core.windows.net";
 
             //Load the default settings
             Settings.Instance = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(Directory.GetCurrentDirectory() + @"\appsettings.json"));
