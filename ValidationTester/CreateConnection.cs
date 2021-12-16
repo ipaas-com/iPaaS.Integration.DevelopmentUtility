a1f4460e381502a34bfc25b6ec82306ab3d52bef
Vincent Valentine <marjak101@gmail.com>
2021-12-15 17:04:45 -0800

    new files

---
+++ ValidationTester/CreateConnection.cs
@@ -0,0 +1,162 @@
+﻿using IntegrationDevelopmentUtility.iPaaSModels;
+using IntegrationDevelopmentUtility.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationDevelopmentUtility.ValidationTester
+{
+    public class CreateConnection
+    {
+        //This call is responsible for creating an empty, ready-to-use connection. That includes loading the external assembly, instantiating all the types, 
+        //  and assigning delegate function references. 
+        //It does NOT make any external calls. That should all be handled in the ConnectionManager.
+        public static Tuple<Integration.Abstract.Connection, object> Create(string assemblyPath, string dllNamespace, SettingResponse settings, FullToken apiToken, long iPaaSSystemId)
+        {
+            // Use the file name to load the assembly into the current
+            // application domain.
+            var assemblyHandler = new AssemblyHandler(assemblyPath);
+
+            // If we don't have the namespace supplied, guess at it. See the comments on this method for its (severe) limitations
+            if (string.IsNullOrEmpty(dllNamespace))
+                assemblyHandler.DetermineNamespaceByClassName("Connection");
+            else
+                assemblyHandler.ExternalNamespace = dllNamespace;
+
+            // First create the connection
+            var connection = assemblyHandler.CreateInstance<Integration.Abstract.Connection>("Connection");
+
+            // First assign the basic external and internal settings
+            connection.ExternalSystemId = settings.Id;
+            connection.ExternalSystemType = settings.SystemTypeId;
+            connection.IPaaSApiToken = apiToken.AcessToken;
+            connection.IPaaSSystemId = iPaaSSystemId;
+
+            // Populate each of these objects: CallWrapper, Settings, TranslationUtilities, CustomFieldHandler
+            connection.Settings = assemblyHandler.CreateInstance<Integration.Abstract.Settings>("Settings");
+            foreach (var kvp in settings.Settings)
+                connection.Settings.SettingsDictionary.TryAdd(kvp.Key, kvp.Value);
+
+            // Dont be lazy and comment more please
+            connection.TranslationUtilities = assemblyHandler.CreateInstance<Integration.Abstract.TranslationUtilities>("TranslationUtilities");
+
+            // Populating the CustomFieldHandler
+            connection.CustomFieldHandler = assemblyHandler.CreateInstance<Integration.Abstract.CustomFieldHandler>("CustomFieldHandler");
+
+            // Create and initialize the callwrapper. Note that the EstablishConnection call is optionally implemented. The abstract class contains it as an empty function, so integrations
+            // can choose to use it or not.
+            connection.CallWrapper = assemblyHandler.CreateInstance<Integration.Abstract.CallWrapper>("CallWrapper");
+
+            // Note that for ConversionFunctions, we only store the type, not an instance of the type. This is because the ConversionFunctions are all static methods (as required
+            //  by FLEE)
+            connection.ConversionFunctionType = assemblyHandler.GetType("ConversionFunctions");
+            connection.TranslationUtilitiesType = assemblyHandler.GetType("TranslationUtilities");
+
+            // Check for a persistent data class. This is actually optional in the external dll, so if does not exist, we will create an instance of the abstract version
+            connection.Settings.PersistentData = assemblyHandler.CreateInstance<Integration.Abstract.Helpers.PersistentDataHandler>("PersistentDataHandler", isOptional: true);
+            if (connection.Settings.PersistentData == null)
+                connection.Settings.PersistentData = new Integration.Abstract.Helpers.PersistentDataHandler();
+
+            //Convert any settings.PersistentData to the correct format
+            if(settings.PersistentData != null)
+                foreach (var persistentDatum in settings.PersistentData)
+                    connection.Settings.PersistentData.Values.Add(new Integration.Abstract.Model.PersistentData() { Name = persistentDatum.Name, Value = persistentDatum.Value, ExpirationDateTime = persistentDatum.ExpirationDateTime });
+
+            ApplySettings(connection.Settings, apiToken.AcessToken);
+
+            //There are a couple of settings fields that we do not populate from the settings dictionary:
+            connection.Settings.Id = settings.Id;
+            connection.Settings.SystemTypeId = settings.SystemTypeId;
+            connection.Settings.Name = settings.Name;
+            connection.Settings.WebhookApiKey = settings.WebhookApiKey;
+
+            // Now we register the delegate functions
+            connection.DataHandlerFunctionAsync = CreateConnection.ExternalDataHandlerAsync;
+            connection.ClapbackTrackerFunctionAsync = RegisterClapback_Delegate;
+
+            connection.Logger = new Integration.Abstract.Helpers.Logger();
+            connection.Logger.Logger_Technical = LogTechEvent_Delegate;
+            //TODO: Logger activity tracker
+
+            //We store the assemblyHandler so that we can call the Unload() method once we are done.
+            connection.Assembly = assemblyHandler;
+
+            // Establish a connection to the call wrapper
+            dynamic dynamicConnection = connection;
+            dynamicConnection.CallWrapper.EstablishConnection(connection, connection.Settings);
+
+            // Save Persistent Data (check to see if there is any data)
+            if (connection.Settings.PersistentData != null && connection.Settings.PersistentData.Values != null && connection.Settings.PersistentData.Values.Count > 0)
+                iPaaSCallWrapper.PersistentData(connection.ExternalSystemId, connection.Settings.PersistentData.Values, apiToken);
+
+            //Now create a DevelopmentTests class
+            var obj = assemblyHandler.CreateInstance<object>("DevelopmentTests", isOptional: true);
+
+            return new Tuple<Integration.Abstract.Connection, object>(connection, obj);
+        }
+
+        /// <summary>
+        /// Apply the context settings to the connection-specific abstract settings class. This allows external dlls to know the URLs for iPaaS API calls
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <param name="context"></param>
+        private static void ApplySettings(Integration.Abstract.Settings settings, string apiToken)
+        {
+            //settings.SettingsDictionary.TryAdd("Customers_URL", context.DatabaseSettings.ApiUrls.Tenant_URL);
+            //settings.SettingsDictionary.TryAdd("Giftcards_URL", context.DatabaseSettings.ApiUrls.Krennic_URL);
+            //settings.SettingsDictionary.TryAdd("Products_URL", context.DatabaseSettings.ApiUrls.Tagge_URL);
+            //settings.SettingsDictionary.TryAdd("Transactions_URL", context.DatabaseSettings.ApiUrls.Motti_URL);
+            if (settings.SettingsDictionary == null)
+            {
+                //settings.SettingsDictionary = new System.Collections.Concurrent.ConcurrentDictionary<string, string>();
+                settings.SettingsDictionary = new Dictionary<string, string>();
+            }
+                
+
+            settings.SettingsDictionary.TryAdd("Integrations_URL", Utilities.Settings.Instance.IntegrationUrl);
+            settings.SettingsDictionary.TryAdd("SSO_URL", Utilities.Settings.Instance.SSOUrl);
+            settings.SettingsDictionary.TryAdd("IPaaSApi_Token", apiToken);
+            settings.SettingsDictionary.TryAdd("Hook_URL", Utilities.Settings.Instance.HookUrl);
+
+            settings.SettingsDictionary.TryAdd("Customers_URL", Utilities.Settings.Instance.CustomerURL);
+            settings.SettingsDictionary.TryAdd("GiftCards_URL", Utilities.Settings.Instance.GiftCardUrl);
+            settings.SettingsDictionary.TryAdd("Products_URL", Utilities.Settings.Instance.ProductUrl);
+            settings.SettingsDictionary.TryAdd("Transactions_URL", Utilities.Settings.Instance.TransactionUrl);
+        }
+
+        public static async Task ExternalDataHandlerAsync(Integration.Abstract.Helpers.TransferRequest transferRequest)
+        {
+            ;
+        }
+
+        public static async Task RegisterClapback_Delegate(long systemId, int mappingCollectionTypeId, string id, int directionId)
+        {
+            ;
+        }
+
+        public static void LogTechEvent_Delegate(string severity, string location, string message)
+        {
+            Utilities.StandardUtilities.Severity level;
+            switch (severity)
+            {
+                case "V":
+                    level = Utilities.StandardUtilities.Severity.VERBOSE;
+                    break;
+                case "D":
+                    level = Utilities.StandardUtilities.Severity.DETAIL;
+                    break;
+                case "W":
+                    level = Utilities.StandardUtilities.Severity.WARNING;
+                    break;
+                case "E":
+                    level = Utilities.StandardUtilities.Severity.ERROR;
+                    break;
+                default:
+                    throw new Exception("Invalid severity level. Valid values are: V (Verbose), D (Detail), W (Warning), or E (Error)");
+            }
+
+            Utilities.StandardUtilities.WriteToConsole(location + " " + message, level);
+        }
+    }
+}
---
+++ ValidationTester/DevelopmentTester.cs
@@ -0,0 +1,107 @@
+﻿using IntegrationDevelopmentUtility.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationDevelopmentUtility.ValidationTester
+{
+    public class DevelopmentTester
+    {
+        public static void ExecuteTestCase(string methodName, long systemId)
+        {
+            var system = Settings.Instance.Systems.Find(x => x.Id == systemId);
+            if(system == null)
+            {
+                if (systemId == 0)
+                    StandardUtilities.WriteToConsole($"System 0 specified but no system was specified in the configuration. See the documentaiton for more details on this feature.", StandardUtilities.Severity.LOCAL_ERROR);
+                else
+                    StandardUtilities.WriteToConsole($"You do not have access to the system specified: {systemId}", StandardUtilities.Severity.LOCAL_ERROR);
+
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                return;
+            }
+
+            var company = Settings.Instance.Companies.Find(x => x.Systems.Exists(y => y.Id == systemId));
+
+            if(string.IsNullOrEmpty(Settings.Instance.IntegrationFileLocation))
+            {
+                StandardUtilities.WriteToConsole("No filename specified. TEST requires a file specified in integration_file_location in the config file.", StandardUtilities.Severity.LOCAL_ERROR);
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                return;
+
+            }
+
+            Integration.Abstract.Connection connection = null;
+            object devTests = null;
+
+            //UploadFile(fileLocation);
+            try
+            {
+                var tupleResults = ValidationTester.CreateConnection.Create(Settings.Instance.IntegrationFileLocation, "", system, company.CompanySpecificFullToken, company.iPaaSSystemId);
+                connection = tupleResults.Item1;
+                devTests = tupleResults.Item2;
+            }
+            catch(Exception ex)
+            {
+                StandardUtilities.WriteToConsole($"Unable to create a connection to the system specified: {systemId}", StandardUtilities.Severity.LOCAL_ERROR);
+                StandardUtilities.WriteToConsole(ex, StandardUtilities.Severity.LOCAL_ERROR);
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                return;
+            }
+
+            if(!connection.CallWrapper.Connected)
+            {
+                StandardUtilities.WriteToConsole($"Unable to connect to the system specified: {systemId}", StandardUtilities.Severity.LOCAL_ERROR);
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                return;
+            }
+
+            if(devTests == null)
+            {
+                StandardUtilities.WriteToConsole($"The integration file does not include a class for named DevelopmentTests or it is not in the standard namespace. See the documentation for more details.", StandardUtilities.Severity.LOCAL_ERROR);
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                return;
+            }
+
+            Type devTestType = devTests.GetType();
+            MethodInfo theMethod = devTestType.GetMethod(methodName);
+            if(theMethod == null)
+            {
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                StandardUtilities.WriteToConsole("The method name specified does not exist.", StandardUtilities.Severity.LOCAL_ERROR);
+                return;
+            }
+
+            var theMethodParameters = theMethod.GetParameters();
+            if(theMethodParameters.Length != 1)
+            {
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                StandardUtilities.WriteToConsole("The method must accept only one parameter.", StandardUtilities.Severity.LOCAL_ERROR);
+                return;
+            }
+
+            if(theMethodParameters[0].ParameterType != typeof(Integration.Abstract.Connection))
+            {
+                Program.OperationCancelled = true;
+                Program.OperationCompleted = true;
+                StandardUtilities.WriteToConsole("The method must accept only one parameter of the type Integration.Abstract.Connection.", StandardUtilities.Severity.LOCAL_ERROR);
+                return;
+            }
+
+            var methodParams = new object[1];
+            methodParams[0] = connection;
+
+            Task result = (Task)theMethod.Invoke(devTests, methodParams);
+            result.GetAwaiter().GetResult();
+        }
+    }
+}
