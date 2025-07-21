e0393ad7665a5622e118d6831f63e768c815923a
Vance Berisford <vanceho@hotmail.com>
2025-07-21 17:36:43 -0400

    Add files via upload
    Comment this file out as the model has been replaced

--- iPaaSModels/UserCompanyResponse.cs
+++ iPaaSModels/UserCompanyResponse.cs
@@ -1,27 +1,27 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using Newtonsoft.Json;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace IntegrationDevelopmentUtility.iPaaSModels
-{
-    public class UserCompanyResponse
-    {
-        #region Properties
-        [JsonProperty("admin_companies", Order = 10)]
-        public List<CompanyInfoResponse> AdminCompanies { get; set; }
+//namespace IntegrationDevelopmentUtility.iPaaSModels
+//{
+//    public class UserCompanyResponse
+//    {
+//        #region Properties
+//        [JsonProperty("admin_companies", Order = 10)]
+//        public List<CompanyInfoResponse> AdminCompanies { get; set; }
 
-        [JsonProperty("companies", Order = 15)]
-        public List<CompanyInfoResponse> Companies { get; set; }
+//        [JsonProperty("companies", Order = 15)]
+//        public List<CompanyInfoResponse> Companies { get; set; }
 
-        [JsonProperty("misps", Order = 20)]
-        public List<CompanyInfoResponse> MISPs { get; set; }
+//        [JsonProperty("misps", Order = 20)]
+//        public List<CompanyInfoResponse> MISPs { get; set; }
 
-        [JsonProperty("tech_partners", Order = 25)]
-        public List<CompanyInfoResponse> TechPartners { get; set; }
+//        [JsonProperty("tech_partners", Order = 25)]
+//        public List<CompanyInfoResponse> TechPartners { get; set; }
 
-        [JsonProperty("integrators", Order = 30)]
-        public List<CompanyInfoResponse> Integrators { get; set; }
-        #endregion
-    }
-}
+//        [JsonProperty("integrators", Order = 30)]
+//        public List<CompanyInfoResponse> Integrators { get; set; }
+//        #endregion
+//    }
+//}
